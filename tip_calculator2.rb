ccb643d6fe0a539980827b629858d6d8531824c0
Evan Lloyd <eulloyd@gmail.com>
2013-12-20 14:53:24 +0900

    Refactor float on gets

--- tip_calculator2.rb
+++ tip_calculator2.rb
@@ -1,16 +1,14 @@
 puts "Enter the cost of your meal without tax as a plain number (no $ sign please!)"
-meals = gets.chomp!
-meal = Float(meals)
+meal = Float(gets.chomp!)
+
 puts "Enter the tax rate for your meal as a decimal value (i.e. 0.05 for 5%)" 
-taxs = gets.chomp!
-tax = Float(taxs)
+tax = Float(gets.chomp!)
+
 puts "Enter the tip rate for your meal as a decimal value (i.e. 0.15 for 15%)"
-tips = gets.chomp!
-tip = Float(tips)
-#breaks because can't do math on strings! use float to change?
+tip = Float(gets.chomp!)
+
 tax_value = meal * tax  
 meal_with_tax = meal + tax_value
-# puts meal_with_tax
 tip_value = meal * tip
 total_cost = meal_with_tax + tip_value
 
