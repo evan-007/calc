472e7421850777e3b91a8ade1ebdc4d1a264534c
Evan Lloyd <eulloyd@gmail.com>
2013-12-20 14:38:42 +0900

    Calc 2 takes user input

--- tip_calculator2.rb
+++ tip_calculator2.rb
@@ -1,11 +1,17 @@
-meal = 20 #price of meal 
-tax = 0.05 #tax rate
-tip = 0.15 #tip rate
-
+puts "Enter the cost of your meal without tax as a plain number (no $ sign please!)"
+meals = gets.chomp!
+meal = Float(meals)
+puts "Enter the tax rate for your meal as a decimal value (i.e. 0.05 for 5%)" 
+taxs = gets.chomp!
+tax = Float(taxs)
+puts "Enter the tip rate for your meal as a decimal value (i.e. 0.15 for 15%)"
+tips = gets.chomp!
+tip = Float(tips)
+#breaks because can't do math on strings! use float to change?
 tax_value = meal * tax  
 meal_with_tax = meal + tax_value
 # puts meal_with_tax
 tip_value = meal * tip
 total_cost = meal_with_tax + tip_value
 
-puts total_cost
+puts "The tip is #{tip_value}, the meal with taxes is #{meal_with_tax} and the total damage to your wallet is #{total_cost}."
