34e3b44263b587c65e460134428199b40b2951c8
Evan Lloyd <eulloyd@gmail.com>
2013-12-21 13:56:21 +0900

    Add value method

--- tip_calculator3.rb
+++ tip_calculator3.rb
@@ -1,4 +1,4 @@
-puts "Enter the cost of your meal without tax as a plain number (no $ sign please!)"
+puts "Enter the cost of your meal without tax (100 for $100)"
 meal = Float(gets.chomp!)
 
 puts "Enter the tax rate for your meal(5 for 5%)" 
@@ -7,14 +7,13 @@ tax = (Float(gets.chomp!))/100
 puts "Enter the tip rate for your meal(15 for 15%)"
 tip = (Float(gets.chomp!))/100
 
-#def value(num)
-#return meal * num
-#end
-#value(tax)
-#value(tip)
-tax_value = (meal * tax).round(2)  
-meal_with_tax = (meal + tax_value).round(2)
-tip_value = (meal * tip).round(2)
-total_cost = (meal_with_tax + tip_value).round(2)
+#calculates the 
+def value(number, percent)
+	(number * percent).round(2)
+end
 
-puts "The tip is #{tip_value}, the meal with taxes is #{meal_with_tax} and the total damage to your wallet is #{total_cost}."
+value(meal, tax)
+
+total_cost = meal + value(meal, tax) + value(meal, tip)
+#how to make total cost always show 2 decimal places even when they are zero?
+puts "The tip is $#{value(meal, tip)}, the taxes are $#{value(meal, tax)} and the total damage to your wallet is $#{total_cost}."
